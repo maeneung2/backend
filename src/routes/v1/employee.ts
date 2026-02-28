8726e4c235b9b383ca50bf4676fcc0079605e6c8
MaeNeung <maeneung@kakao.com>
2026-02-28 11:03:59 +0900

    add workflow #1

--- src/routes/v1/employee.ts
+++ src/routes/v1/employee.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import prisma from "../../prisma";
 
 const router = express.Router();
 
@@ -16,61 +15,61 @@ router.post("/", async (req, res) => {
     return;
   }
 
-  try {
-    const emp = await prisma.employee.create({
-      data: {
-        name: name,
-        workCount: workCount,
-        isNew: isNew,
-        plan: [],
-        scheduleId: scheduleId,
-      },
-    });
-    console.log("생성된 유저:", emp);
-    res.send({ data: emp });
-  } catch (err) {
-    console.error(err);
-    res.send({ message: "유저 생성에 실패했습니다.", error: err });
-  }
+  // try {
+  //   const emp = await prisma.employee.create({
+  //     data: {
+  //       name: name,
+  //       workCount: workCount,
+  //       isNew: isNew,
+  //       plan: [],
+  //       scheduleId: scheduleId,
+  //     },
+  //   });
+  //   console.log("생성된 유저:", emp);
+  //   res.send({ data: emp });
+  // } catch (err) {
+  //   console.error(err);
+  //   res.send({ message: "유저 생성에 실패했습니다.", error: err });
+  // }
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const emp = await prisma.employee.delete({
-      where: { id: id },
-    });
-    console.log("생성된 유저:", emp);
-    res.send({ data: emp });
-  } catch (err) {
-    console.error(err);
-    res.send({ message: "유저 삭제에 실패했습니다.", error: err });
-  }
+  // try {
+  //   const id = Number.parseInt(req.params.id);
+  //   const emp = await prisma.employee.delete({
+  //     where: { id: id },
+  //   });
+  //   console.log("생성된 유저:", emp);
+  //   res.send({ data: emp });
+  // } catch (err) {
+  //   console.error(err);
+  //   res.send({ message: "유저 삭제에 실패했습니다.", error: err });
+  // }
 });
 
 router.get("/list", async (req, res) => {
-  try {
-    const employee = await prisma.employee.findMany();
-    res.send({ data: employee });
-  } catch (err) {
-    res.send({
-      message: "유저 호출에 실패하였거나, 없는 유저입니다.",
-      error: err,
-    });
-  }
+  // try {
+  //   const employee = await prisma.employee.findMany();
+  //   res.send({ data: employee });
+  // } catch (err) {
+  //   res.send({
+  //     message: "유저 호출에 실패하였거나, 없는 유저입니다.",
+  //     error: err,
+  //   });
+  // }
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const employee = await prisma.employee.findUnique({ where: { id: id } });
-    res.send({ data: employee });
-  } catch (err) {
-    res.send({
-      message: "유저 호출에 실패하였거나, 없는 유저입니다.",
-      error: err,
-    });
-  }
+  // try {
+  //   const id = Number.parseInt(req.params.id);
+  //   const employee = await prisma.employee.findUnique({ where: { id: id } });
+  //   res.send({ data: employee });
+  // } catch (err) {
+  //   res.send({
+  //     message: "유저 호출에 실패하였거나, 없는 유저입니다.",
+  //     error: err,
+  //   });
+  // }
 });
 
 export default router;
--- src/routes/v1/schedule/index.ts
+++ src/routes/v1/schedule/index.ts
@@ -1,20 +1,18 @@
 import express from "express";
-import prisma from "../../../prisma";
-import { ScheduleCreateInput } from "../../../generated/prisma/models/Schedule";
 
 const router = express.Router();
 
 router.get("/:id", async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const schedule = await prisma.schedule.findUnique({ where: { id: id } });
-    res.send({ data: schedule });
-  } catch (err) {
-    res.send({
-      message: "없는 스케줄이거나 스케줄을 불러오는데 실패했습니다.",
-      error: err,
-    });
-  }
+  // try {
+  //   const id = parseInt(req.params.id);
+  //   const schedule = await prisma.schedule.findUnique({ where: { id: id } });
+  //   res.send({ data: schedule });
+  // } catch (err) {
+  //   res.send({
+  //     message: "없는 스케줄이거나 스케줄을 불러오는데 실패했습니다.",
+  //     error: err,
+  //   });
+  // }
 });
 
 router.post("/", async (req, res) => {
