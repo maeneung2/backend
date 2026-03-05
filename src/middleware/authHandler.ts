b875d8ae85f96a6bfc0ad47dc0b2340f39e29470
MaeNeung <maeneung@kakao.com>
2026-03-05 18:46:41 +0900

    서버 만료 오류코드 변경

--- src/middleware/authHandler.ts
+++ src/middleware/authHandler.ts
@@ -17,7 +17,7 @@ export const authenticateToken = (
 
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ error: "Invalid or expired token" });
+      return res.status(401).json({ error: "Invalid or expired token" });
     }
 
     req.user = decoded as Request["user"];
